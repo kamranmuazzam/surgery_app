90bc6d9acaa622efb7850b101e371cba022eed3f
Kamran <kamranmuazzam@gmail.com>
2025-09-10 16:37:09 +0600

    initializing

---
+++ src-tauri/gen/android/app/src/main/java/com/kamran/surgery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kamran.surgery
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
